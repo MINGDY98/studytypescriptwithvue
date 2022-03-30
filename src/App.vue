88c2f13211801b589c71b69dda1f02187622bc21
mingdy98 <alswl1500@naver.com>
2022-03-30 21:43:29 +0900

    [feat] 할일 관리앱 - 조회 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,21 @@
       <h1>vue Todo with Typescript</h1>
     </header>
     <main>
-      <todo-input
+      <TodoInput
         :item="todoText"
         @input="updateTodoText"
         @add="addTodoItem"
-      ></todo-input>
+      ></TodoInput>
       <div>
         <ul>
-          <li>아이템 1</li>
+          <TodoListItem
+            v-for="(todoItem, index) in todoItems"
+            :key="index"
+            :todoItem="todoItem"
+          ></TodoListItem>
+          <!-- <li>아이템 1</li>
           <li>아이템 2</li>
-          <li>아이템 3</li>
+          <li>아이템 3</li> -->
         </ul>
       </div>
     </main>
@@ -23,12 +28,36 @@
 <script lang="ts">
 import Vue from "vue";
 import TodoInput from "./components/TodoInput.vue";
+import TodoListItem from "./components/TodoListItem.vue";
 
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  save(todoItems: any[]) {
+    //직렬화 : 배열=> string 변환
+    const parsed = JSON.stringify(todoItems);
+    localStorage.setItem(STORAGE_KEY, parsed);
+  },
+  fetch() {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
+    const result = JSON.parse(todoItems);
+    /**
+     * Argument of type 'string | never[]' is not assignable to parameter of type 'string'.
+  Type 'never[]' is not assignable to type 'string
+     * 타입 오류가 나는 이유.
+     * JSON.parse의 첫번째 파라메터가 string이라고 되어있기 때문 (parse에 마우스 올리면 알수있음)
+     * 그래서 JSON.parse안에 들어갈 것은 string이거나 string 하위 타입이여야함
+     * []를 '[]'로 바꿈으로서 문제해결
+     *
+     */
+    return result;
+  },
+};
 export default Vue.extend({
-  components: { TodoInput },
+  components: { TodoInput, TodoListItem },
   data() {
     return {
       todoText: "",
+      todoItems: [] as any[],
     };
   },
   methods: {
@@ -37,12 +66,21 @@ export default Vue.extend({
     },
     addTodoItem() {
       const value = this.todoText;
-      localStorage.setItem(value, value);
+      //localStorage.setItem(value, value);
+      this.todoItems.push(value);
+      storage.save(this.todoItems);
       this.initTodoText();
     },
     initTodoText() {
       this.todoText = "";
     },
+    fetchTodoItems() {
+      this.todoItems = storage.fetch();
+      //todoItems가 never[]로되어있는데 그 이유는 data에 []만 초기화시키고 타입을 지정안해서!
+    },
+  },
+  created() {
+    this.fetchTodoItems();
   },
 });
 </script>
